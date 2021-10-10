b9834f598668938ee5bc016771c7339bac731936
axiboi <68826294+axiboi@users.noreply.github.com>
2021-10-10 17:19:26 -0500

    Updated Code
    Added additional sensor information for redundancy

--- water-code.ino
+++ water-code.ino
@@ -1,31 +1,56 @@
 int firstSensor = A3;   
-int firstPin = 10;          
-int firstVal= 2;      
-int firstSensorValue= 0;     
+int firstPin = 10;
+int firstVal= 2;
+int firstSensorValue= 0;   
+
+int secondSensor = A4;    
+int secondPin = 12;
+int secondVal = 4;  
+int secondSensorValue = 0;    
 
 void setup() {
   pinMode(firstSensor, INPUT);
   pinMode(firstPin, OUTPUT);
+  pinMode(secondSensor, INPUT);
+  pinMode(secondPin, OUTPUT);
   pinMode(ValveOutput1, OUTPUT);
+  pinMode(secondVal, OUTPUT);
 
-  Serial.begin(9600);
+  Serial.begin(9000);
 }
 
 void loop() {
   digitalWrite(firstPin, HIGH);
-  delay(100);
+  delay(50);
   firstSensorValue = analogRead(firstSensor);
-  delay(100);
+  delay(50);
   Serial.println(sensorValue1);
   digitalWrite(firstPin, LOW);
-  delay(250);
-  
-  if (firstSensorValue>= 700) {
+  delay(100);
+
+  digitalWrite(secondPin, HIGH);
+  delay(50);
+  secondSensorValue = analogRead(secondSensor);
+  delay(50);
+  Serial.println(secondSensorValue);
+  digitalWrite(secondPin, LOW);
+  delay(100);
+
+
+  if (firstSensorValue >= 550) {
     digitalWrite(ValveOutput1, HIGH);
   }
 
-  if (firstSensorValue< 700) {
+  if (firstSensorValue < 550) {
     digitalWrite(ValveOutput1, LOW);
+  }
+
+  if (secondSensorValue >= 550) {
+    digitalWrite(secondVal, HIGH);
+  }
+
+  if (secondSensorValue < 550) {
+    digitalWrite(secondVal, LOW);
   }
   
   delay(500);
